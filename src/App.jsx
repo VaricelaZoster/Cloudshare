bc00c7b854db0b54c096fd807e1d877c21897112
VaricelaZoster <162027550+VaricelaZoster@users.noreply.github.com>
2025-12-09 16:32:47 +0530

    Added pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Landing from './pages/Landing'
+import Dashboard from './pages/Dashboard'
+import Upload from './pages/Upload'
+import MyFiles from './pages/MyFiles'
+import Subscription from './pages/Subscription'
+import Transactions from './pages/Transactions'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/myfiles" element={<MyFiles />} />
+        <Route path="/publicfilesview" element={<Landing />} />
+        <Route path="/subscriptions" element={<Subscription />} />
+        <Route path="/Transactions" element={<Transactions />} />
+        <Route path="/upload" element={<Upload />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
